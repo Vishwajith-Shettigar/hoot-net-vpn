2437bee341d432ff2c5fc67e06471c9f902ec64e
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2025-05-24 18:03:39 +0530

    UI

--- app/src/main/java/com/example/hoot_net/screens/MainScreen.kt
+++ app/src/main/java/com/example/hoot_net/screens/MainScreen.kt
@@ -1,8 +1,12 @@
 package com.example.hoot_net.screens
 
+import android.graphics.Paint
+import android.text.Layout
+import android.widget.Space
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
@@ -19,76 +23,97 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.BottomSheetScaffoldState
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SegmentedButtonDefaults.Icon
-import androidx.compose.material3.SheetState
+import androidx.compose.material3.Icon
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.colorspace.WhitePoint
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Density
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.hoot_net.R
 import com.example.hoot_net.ui.theme.cherryBomb
-import kotlinx.coroutines.launch
+
+val grayscaleColors = listOf(
+  Color.Gray,
+  Color.LightGray,
+  Color.Gray
+)
+
+val rgbColors = listOf(
+  Color.Red,
+  Color.Blue,
+  Color.Green
+)
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(modifier: Modifier) {
   val scope = rememberCoroutineScope()
 
+  val waveColors by remember {
+    mutableStateOf<List<Color>>(rgbColors)
+  }
+
+  val ringColors by remember {
+    mutableStateOf<List<Color>>(rgbColors)
+
+  }
+
   val bottomSheetState = rememberBottomSheetScaffoldState(
     bottomSheetState = rememberStandardBottomSheetState(
       initialValue = SheetValue.PartiallyExpanded,
-      skipHiddenState = false
+      skipHiddenState = true
     )
   )
   BottomSheetScaffold(
-    scaffoldState = bottomSheetState, sheetPeekHeight = 100.dp,
+    scaffoldState = bottomSheetState, sheetPeekHeight = 160.dp,
     sheetContainerColor = Color.Blue.copy(alpha = 0.35f), sheetContent = {
-      Column(
-        Modifier
-          .fillMaxWidth()
-          .height(200.dp)
-          .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-      ) {
-        Text("VPN Status", fontWeight = FontWeight.Bold)
-        Text("Server: Netherlands")
-      }
-      Column(
-        Modifier
-          .fillMaxWidth()
-          .height(200.dp)
-          .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-      ) {
-        Text("VPN Status", fontWeight = FontWeight.Bold)
-        Text("Server: Netherlands")
+      LazyColumn {
+        items(49) {
+          RegionCard()
+          Spacer(modifier = Modifier.height(10.dp))
+        }
       }
     },
-    sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+    sheetShape = RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp)
   ) {
     Box(
       modifier = Modifier
@@ -121,17 +146,60 @@ fun MainScreen(modifier: Modifier) {
           )
         }
       }
-      WaveGlowButton({}, modifier = Modifier.align(Alignment.Center))
+      val composition by rememberLottieComposition(
+        LottieCompositionSpec.Asset("jumpingbird.json")
+      )
+      WaveGlowButton(
+        onClick = {},
+        modifier = Modifier
+          .align(Alignment.Center)
+          .offset(y = -50.dp),
+        waveColors,
+        ringColors
+      )
+
+      Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+          .align(Alignment.BottomCenter)
+          .offset(y = -260.dp),
+      ) {
+        Text(
+          text = "Swipe up to select a region",
+          color = Color.LightGray
+        )
+      }
+      LottieAnimation(
+        modifier = Modifier
+          .size(200.dp)
+          .align(Alignment.BottomCenter)
+          .offset(y = -140.dp),
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+      )
+
     }
   }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun WaveGlowButton(
   onClick: () -> Unit,
-  modifier: Modifier = Modifier
+  modifier: Modifier = Modifier, waveColors: List<Color>, ringColors: List<Color>
 ) {
   val infiniteTransition = rememberInfiniteTransition()
+
+
+  var isPressed by remember {
+    mutableStateOf(false)
+  }
+
+  val scale by animateFloatAsState(
+    targetValue = if (isPressed) 0.92f else 1f,
+    animationSpec = tween(100),
+    label = "Press animation"
+  )
 
   val wave1Radius = infiniteTransition.animateFloat(
     initialValue = 0f,
@@ -168,7 +236,6 @@ fun WaveGlowButton(
     )
   )
 
-
   Box(
     contentAlignment = Alignment.Center,
     modifier = modifier.size(200.dp)
@@ -176,19 +243,19 @@ fun WaveGlowButton(
     // Wave 1
     Canvas(modifier = Modifier.fillMaxSize()) {
       drawCircle(
-        color = Color.Red,
+        color = waveColors[0],
         radius = wave1Radius.value,
         center = center,
         alpha = waveAlpha.value
       )
       drawCircle(
-        color = Color.Blue,
+        color = waveColors[1],
         radius = wave2Radius.value,
         center = center,
         alpha = waveAlpha.value
       )
       drawCircle(
-        color = Color.Green,
+        color = waveColors[2],
         radius = wave3Radius.value,
         center = center,
         alpha = waveAlpha.value
@@ -196,21 +263,115 @@ fun WaveGlowButton(
     }
     Box(
       modifier = Modifier
+        .graphicsLayer {
+          scaleX = scale
+          scaleY = scale
+        }
         .size(120.dp)
         .clip(CircleShape)
-        .border(width = 2.dp, shape = CircleShape, color = Color.White)
+        .border(
+          width = 3.dp, shape = CircleShape, brush = Brush.linearGradient(
+            colors = listOf(
+              ringColors[0], ringColors[1], ringColors[2]
+            )
+          )
+        )
         .background(Color.LightGray)
-        .clickable { onClick() },
+        .pointerInteropFilter {
+          when (it.action) {
+            android.view.MotionEvent.ACTION_DOWN -> {
+              isPressed = true
+              true
+            }
+
+            android.view.MotionEvent.ACTION_UP, android.view.MotionEvent.ACTION_CANCEL -> {
+              isPressed = false
+              onClick()
+              true
+
+            }
+
+            else -> false
+          }
+        },
       contentAlignment = Alignment.Center
     ) {
-      androidx.compose.material3.Icon(
-        imageVector = Icons.Default.Person,
-        contentDescription = "Power",
-        tint = Color.White,
-        modifier = Modifier.size(32.dp)
+      Image(
+        modifier = Modifier
+          .size(80.dp), painter = painterResource(R.drawable.connected_2), contentDescription = ""
       )
     }
   }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun RegionCard() {
+  var isPressed by remember {
+    mutableStateOf(false)
+  }
 
+  val scale by animateFloatAsState(
+    targetValue = if (isPressed) 0.92f else 1f,
+    animationSpec = tween(100),
+    label = "Press animation"
+  )
+  Row(
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.Start,
+    modifier = Modifier
+      .graphicsLayer {
+        scaleX = scale
+        scaleY = scale
+      }
+      .height(80.dp)
+      .pointerInteropFilter {
+        when (it.action) {
+          android.view.MotionEvent.ACTION_DOWN -> {
+            isPressed = true
+            true
+          }
+
+          android.view.MotionEvent.ACTION_UP, android.view.MotionEvent.ACTION_CANCEL -> {
+            isPressed = false
+            true
+
+          }
+
+          else -> false
+        }
+      }
+      .fillMaxWidth()
+      .padding(horizontal = 10.dp)
+      .clip(shape = RoundedCornerShape(40.dp))
+      .background(color = Color.Black.copy(alpha = 0.3f))
+      .border(width = 1.dp, color = Color.White, shape = RoundedCornerShape(40.dp))
+      .padding(horizontal = 20.dp)
+  ) {
+    Box(modifier = Modifier
+      .size(50.dp)
+      .weight(0.2f)) {
+      Image(
+        modifier = Modifier.align(Alignment.CenterStart),
+        painter = painterResource(R.drawable.`in`), contentDescription = "",
+
+        )
+    }
+
+    Column(modifier = Modifier.weight(0.6f), horizontalAlignment = Alignment.Start) {
+      Text(
+        text = "South-mumbai", style = TextStyle.Default.copy(fontWeight = FontWeight.ExtraBold),
+        fontSize = 18.sp
+      )
+      Text(text = "In", style = TextStyle.Default.copy(fontWeight = FontWeight.Bold))
+    }
+    Image(
+      painter = painterResource(R.drawable.secured),
+      modifier = Modifier
+        .weight(0.2f)
+        .size(35.dp),
+      contentDescription = ""
+    )
+  }
+}
